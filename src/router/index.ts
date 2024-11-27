d6f6a281ebb4a311ce944c124336d7da6656c972
Tadeu17 <17.tadeu.1989@gmail.com>
2024-11-27 21:16:15 +0000

    removed console logs. changed firebase project in firebase

--- src/router/index.ts
+++ src/router/index.ts
@@ -38,16 +38,11 @@ router.beforeEach(async (to: RouteLocationNormalized) => {
   const storeAuth = useStoreAuth()
   // if the user is not logged in as is trying to go anywhere other than auth route
   if (!storeAuth.user?.id && to.name !== 'auth') {
-    console.log('redirect auth');
-
     return { name: 'auth' }
   }
 
   // if logged in and trying to access auth page
-  console.log('my user', storeAuth.user);
-
   if (storeAuth.user?.id && to.name === 'auth') {
-    console.log('redirect false');
     return false //keep the user in the same page, without redirecting anywhere
   }
 })
